5b943dac42ee7e3b075777a6a8b8ccdd1595f6ae
ArzubekDev <arzubekmain@gmail.com>
2025-12-03 16:04:18 +0600

    hotel

--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -10,7 +10,7 @@ const MainLayout: FC<LayoutProps> = ({ children }) => {
   return (
     <div className="flex flex-col min-h-screen bg-gray-50 text-gray-800">
       <Header />
-      <main className="flex-1 container mx-auto px-4 py-6">
+      <main className="flex-1">
         {children}
       </main>
       <Footer />
