df542759a5e36e9b2b8cdebcd01dc3b42751c7ed
chyngyz-sultanaliev <sultanalievcyngyz98@gmail.com>
2025-12-04 22:01:14 +0600

    auth

--- src/components/layouts/mainLayout/MainLayout.tsx
+++ src/components/layouts/mainLayout/MainLayout.tsx
@@ -8,7 +8,7 @@ interface LayoutProps {
 
 const MainLayout: FC<LayoutProps> = ({ children }) => {
   return (
-    <div className="flex flex-col min-h-screen bg-gray-50 text-gray-800">
+    <div className="flex flex-col min-h-screen">
       <Header />
       <main className="flex-1">
         {children}
