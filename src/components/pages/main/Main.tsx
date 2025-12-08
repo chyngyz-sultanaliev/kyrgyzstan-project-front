b889b3c271c516bb69462651c05719648ef53c3d
Aisuluu <aisuluu@MacBook-Air-Aisuluu.local>
2025-12-08 12:27:06 +0600

    ready

--- src/components/pages/main/Main.tsx
+++ src/components/pages/main/Main.tsx
@@ -1,32 +1,16 @@
-<<<<<<< Updated upstream
+import Car from "./Car";
 import Hotel from "./Hotel";
-import Car from "./Car";
 import News from "./News";
 import Welcome from "./Welcome";
-
-=======
-import Car from "./Car";
-import News from "./News";
-import Welcome from "./Welcome";
->>>>>>> Stashed changes
 
 const Main = () => {
   return (
     <>
       <Welcome />
-<<<<<<< Updated upstream
-      <Hotel />
-=======
->>>>>>> Stashed changes
+      <Hotel/>
       <Car />
       <News />
-      
     </>
   );
 };
-
-<<<<<<< Updated upstream
 export default Main;
-=======
-export default Main;
->>>>>>> Stashed changes
