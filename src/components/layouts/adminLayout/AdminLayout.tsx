7fb1a54f32645f16f14d7c1a9632a3ba53c645f5
chyngyz-sultanaliev <sultanalievcyngyz98@gmail.com>
2025-12-13 11:20:30 +0600

    adminProfile

--- src/components/layouts/adminLayout/AdminLayout.tsx
+++ src/components/layouts/adminLayout/AdminLayout.tsx
@@ -1,16 +1,21 @@
-import { FC, ReactNode } from "react";
+"use client";
+import { FC, ReactNode, useState } from "react";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
 
 interface AdminLayoutProps {
   children: ReactNode;
 }
-const AdminLayout: FC<AdminLayoutProps> = async ({ children }) => {
+
+const AdminLayout: FC<AdminLayoutProps> = ({ children }) => {
+  const [open, setOpen] = useState(false);
+
   return (
     <div className="min-h-screen flex flex-col">
-      <Header />
+      <Header open={open} setOpen={setOpen} />
+
       <div className="flex">
-        <Sidebar />
+        <Sidebar open={open} setOpen={setOpen} />
         <main className="flex-1">{children}</main>
       </div>
     </div>
