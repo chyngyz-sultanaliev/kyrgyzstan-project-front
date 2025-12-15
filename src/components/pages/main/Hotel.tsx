a0d60bbe3ff4e169ca08ca5c2cf1cc62dcd23b58
Aisuluu <aisuluu@MacBook-Air-Aisuluu.local>
2025-12-15 16:32:29 +0600

    Adapt main and hotel

--- src/components/pages/main/Hotel.tsx
+++ src/components/pages/main/Hotel.tsx
@@ -1,6 +1,6 @@
 "use client";
-import Link from "next/link";
 import { motion } from "framer-motion";
+import Link from "next/link";
 import { useEffect, useState } from "react";
 
 const Hotel = () => {
@@ -11,11 +11,11 @@ const Hotel = () => {
     },
     {
       title: "Baitur",
-      img: "https://cdn.pixabay.com/photo/2020/01/14/17/17/kyrgyzstan-4765706_640.jpg",
+      img: "https://media.istockphoto.com/id/156209467/photo/horse-and-mountains.jpg?s=612x612&w=0&k=20&c=lrKB7724ExU5tiTOZ9RG03n80QJqufk0GA2bhqE4Pl4=",
     },
     {
       title: "Raduga",
-      img: "https://cdn.britannica.com/45/190645-050-AC4B6D04/Lake-Ysyk-body-water-Kyrgyzstan.jpg",
+      img: "https://cabar.asia/wp-content/uploads/2024/09/Zapovednik-Kyrgyz-Ata-Oshskaya-oblast-Kyrgyzstan.-Foto-PROON-Kyrgyzstan.jpeg",
     },
     {
       title: "Redens",
@@ -23,94 +23,72 @@ const Hotel = () => {
     },
     {
       title: "Kapriz",
-      img: "https://images.unsplash.com/photo-1610720684893-619cd7a5cde5",
+      img: "https://images.unsplash.com/photo-1610720684893-619cd7a5cde5?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8a3lyZ3l6c3RhbnxlbnwwfHwwfHx8MA%3D%3D",
     },
     {
       title: "Orion",
-      img: "https://sputnik.kg/img/102340/99/1023409982_0:182:4752:3168_1920x0_80_0_0_eda7e4f57f52cf019fa10f5fa0c44057.jpg",
+      img: "https://images.locationscout.net/2024/09/ala-kul-lake-viewpoint-kyrgyz-republic-g6rh.jpg?w=1080&q=60",
     },
   ];
 
-  const [active, setActive] = useState(0);
+  const [catIndex, setCatIndex] = useState(0);
 
   useEffect(() => {
-    const timer = setInterval(() => {
-      setActive((i) => (i + 1) % hotels.length);
-    }, 3000);
-    return () => clearInterval(timer);
+    const interval = setInterval(() => {
+      setCatIndex((i) => (i + 1) % hotels.length);
+    }, 5000);
+    return () => clearInterval(interval);
   }, []);
 
-  const visible = [
-    hotels[(active - 1 + hotels.length) % hotels.length],
-    hotels[active],
-    hotels[(active + 1) % hotels.length],
-  ];
-
   return (
-    <section className=" mt-10 sm:mt-20 px-4">
+    <div className="mt-16 sm:mt-20 px-4">
       <motion.div
         initial={{ opacity: 0, y: 50 }}
         whileInView={{ opacity: 1, y: 0 }}
         transition={{ duration: 1 }}
-        className="w-full flex justify-center z-10 my-10 px-4"
+        className="w-full flex justify-center z-10 my-8 sm:my-10"
       >
         <div className="bg-white/20 backdrop-blur-xl px-6 sm:px-10 py-4 rounded-2xl shadow-xl text-center">
-          <h3 className="text-xl sm:text-2xl font-semibold ">
+          <h3 className="text-xl sm:text-2xl font-semibold">
             Hotel in Kyrgyzstan
           </h3>
         </div>
       </motion.div>
 
-      <div className="flex justify-center items-center gap-4 sm:gap-6 mt-10">
-        {visible.map((hotel, i) => {
-          const isActive = i === 1;
-
-          return (
+      <div className="flex flex-wrap gap-5 justify-center">
+        {hotels.map((cat, i) => (
+          <motion.div
+            key={i}
+            animate={{
+              scale: i === catIndex ? 1 : 0.85,
+              opacity: i === catIndex ? 1 : 0.5,
+            }}
+            transition={{ duration: 0.6 }}
+            className="cursor-pointer"
+            onClick={() => setCatIndex(i)}
+          >
             <div
-              key={i}
-              onClick={() =>
-                !isActive &&
-                setActive(
-                  (active + (i === 0 ? -1 : 1) + hotels.length) % hotels.length
-                )
-              }
-              className={`
-                cursor-pointer
-                transition-all duration-500 mt-7
-                ${
-                  isActive
-                    ? "scale-100 opacity-100"
-                    : "scale-90 opacity-50 hidden sm:block"
-                }
-              `}
+              className="bg-white/10 backdrop-blur-xl
+              overflow-hidden w-50 sm:w-[180px] lg:w-[200px]
+              h-[260px] sm:h-[300px] lg:h-[340px] rounded-2xl
+              shadow-xl hover:scale-105 transition-transform duration-300"
             >
-              <div
-                className="
-                  bg-white/10 backdrop-blur-xl
-                  overflow-hidden
-                  w-35 sm:w-48 lg:w-56
-                  h-60 sm:h-72 lg:h-80
-                  rounded-2xl
-                  shadow-xl
-                "
-              >
-                <Link href="/hotel">
-                  <img
-                    src={hotel.img}
-                    alt={hotel.title}
-                    className="w-full h-full object-cover"
-                  />
-                </Link>
-              </div>
-
-              <p className="mt-2 sm:mt-3 text-center text-base sm:text-lg font-medium">
-                {hotel.title}
-              </p>
+              <Link href="/car">
+                <img
+                  src={cat.img}
+                  alt={cat.title}
+                  className="w-full h-full object-cover"
+                />
+              </Link>
             </div>
-          );
-        })}
+
+            <p className="mt-2 sm:mt-3 text-center text-base sm:text-lg lg:text-xl font-medium">
+              {cat.title}
+            </p>
+          </motion.div>
+        ))}
       </div>
-    </section>
+    </div>
   );
 };
 
--- src/components/pages/main/Welcome.tsx
+++ src/components/pages/main/Welcome.tsx
@@ -4,7 +4,7 @@ import { motion } from "framer-motion";
 import { ChevronRight } from "lucide-react";
 import Link from "next/link";
 
-export default function TourismLanding() {
+export default function Welcome() {
   const backgrounds = [
     "https://images.unsplash.com/photo-1610720684893-619cd7a5cde5?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8a3lyZ3l6c3RhbnxlbnwwfHwwfHx8MA%3D%3D",
     "https://ipt.images.tshiftcdn.com/204440/x/0/12-day-kyrgyzstan-photo-tour-mountains-lakes-canyons.jpg?dpr=2&height=360&quality=65",
@@ -17,26 +17,32 @@ export default function TourismLanding() {
     {
       title: "group-tours",
       img: "https://t3.ftcdn.net/jpg/01/72/11/78/360_F_172117865_2JkKUkVzOG93FVeZIK7gHkxmvmD8JbOB.jpg",
+      full: "Issyk-Kul Lake is the second largest mountain lake in the world at an altitude of 1,607 meters above sea level, with a depth of 668 meters. The lake is situated within the Issyk-Kul Biosphere Reserve, a protected area recognized for its unique ecosystems. Issyk-Kul Lake stretches 182 kilometers in length and about 60 kilometers in width, covering an area of 6,236 square kilometers and serving as one of Central Asia’s most important natural landmarks.",
     },
     {
       title: "horseback-riding",
       img: "https://media.istockphoto.com/id/156209467/photo/horse-and-mountains.jpg?s=612x612&w=0&k=20&c=lrKB7724ExU5tiTOZ9RG03n80QJqufk0GA2bhqE4Pl4=",
+      full: "Horseback riding tours in Kyrgyzstan offer a unique way to explore remote alpine valleys, high mountain pastures and ancient nomadic routes. Travelers experience traditional horse culture, stay in yurts and traverse breathtaking landscapes that are inaccessible by vehicle, making these tours an authentic and immersive adventure.",
     },
     {
       title: "excursions",
       img: "https://cabar.asia/wp-content/uploads/2024/09/Zapovednik-Kyrgyz-Ata-Oshskaya-oblast-Kyrgyzstan.-Foto-PROON-Kyrgyzstan.jpeg",
+      full: "Excursion tours across Kyrgyzstan combine natural beauty, cultural heritage and historical exploration. These journeys include visits to national parks, mountain gorges, ancient caravan routes and UNESCO-recognized sites, offering deep insight into the country’s history, traditions and diverse landscapes.",
     },
     {
-      title: "ethno-tour",
+      title: "ethno-tourism",
       img: "https://eurasia.travel/wp-content/uploads/2024/09/kyrgyzstan-nature-3-1024x683.jpg",
+      full: "Ethno-tourism in Kyrgyzstan introduces travelers to the traditional nomadic lifestyle through stays in yurts, participation in local festivals and hands-on experiences with crafts, music and cuisine. These tours preserve cultural heritage while offering meaningful connections with local communities.",
     },
     {
       title: "hiking",
-      img: "https://images.unsplash.com/photo-1610720684893-619cd7a5cde5?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8a3lyZ3l6c3RhbnxlbnwwfHwwfHx8MA%3D%3D",
+      img: "https://images.unsplash.com/photo-1610720684893-619cd7a5cde5",
+      full: "Hiking tours explore Kyrgyzstan’s diverse mountain terrain, including alpine lakes, forested valleys and high-altitude passes. Routes vary from easy day hikes to multi-day treks, providing opportunities to experience pristine nature, wildlife and panoramic mountain scenery.",
     },
     {
-      title: " ountaineering",
+      title: "mountaineering",
       img: "https://images.locationscout.net/2024/09/ala-kul-lake-viewpoint-kyrgyz-republic-g6rh.jpg?w=1080&q=60",
+      full: "Mountaineering expeditions in Kyrgyzstan are designed for experienced climbers seeking high-altitude challenges. These tours include ascents of iconic peaks, glacier travel and technical routes, supported by professional guides and safety-focused logistics.",
     },
   ];
 
@@ -52,82 +58,69 @@ export default function TourismLanding() {
   }, []);
 
   return (
-    <>
-      {" "}
-      <div className="relative w-full h-max overflow-hidden text-white">
-        {/* BACKGROUND PARALLAX */}
-        <motion.div
-          key={bgIndex}
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ duration: 1.5 }}
-          style={{
-            backgroundImage: `url(${backgrounds[bgIndex]})`,
-          }}
-          className="absolute inset-0 bg-cover bg-center bg-fixed"
-        />
+    <div className="relative w-full min-h-screen overflow-hidden text-white">
+      <motion.div
+        key={bgIndex}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1.5 }}
+        style={{ backgroundImage: `url(${backgrounds[bgIndex]})` }}
+        className="absolute inset-0 bg-cover bg-center bg-fixed"
+      />
 
-        {/* OVERLAY */}
-        <div className="absolute inset-0 bg-black/60" />
+      <div className="absolute inset-0 bg-black/60" />
 
-        {/* MAIN CONTENT */}
+      <div className="relative z-10  px-4 sm:px-6 md:px-12 lg:px-24 sm:py-16 lg:py-24 grid grid-cols-1 md:grid-cols-2 gap-5 sm:gap-8 lg:gap-12">
+        <div className="max-w-full mt-30 sm:mb-10 lg:max-w-xl ">
+          <h2 className="text-3xl sm:text-4xl lg:text-5xl font-bold mb-6 capitalize">
+            {categories[catIndex].title}
+          </h2>
+          <p className="text-base sm:text-lg lg:text-xl leading-relaxed mb-6">
+            {categories[catIndex].full}
+          </p>
+          <button className="flex items-center gap-3 bg-linear-to-r from-teal-500 to-teal-700 px-6 py-3 rounded-2xl text-lg sm:text-xl font-semibold">
+            book <ChevronRight />
+          </button>
+        </div>
 
-        <div className="relative z-10 px-30 my-50 grid grid-cols-2 gap-30">
-          {/* LEFT TEXT */}
-          <div className="max-w-xl">
-            <h2 className="text-5xl font-bold mb-6">
-              {categories[catIndex].title}
-            </h2>
-            <p className="text-lg leading-relaxed mb-10">
-              Issyk-Kul Lake is the second largest mountain lake in the world at
-              an altitude of 1,607 meters above sea level, depth of 668 meters
-              asl. Lake is situated at the territory of the Issyk-Kul Biosphere
-              Reserve. Issyk-Kul Lake is 182 kilometers long and about 60
-              kilometers wide, with an area of 6,236 square kilometers.
-            </p>
-            <button className="flex items-center gap-3 bg-[linear-gradient(rgba(10,156,168,1),rgba(10,135,145,1))] px-6 py-3 rounded-2xl text-xl font-semibold">
-              book <ChevronRight />
-            </button>
-          </div>
-
-          {/* CATEGORY CAROUSEL — 3D INFINITE SPINNING WHEEL */}
-          <div className="relative w-full h-96 flex items-center justify-center">
-            <div
-              className="relative w-[200px] h-[400px]"
-              style={{ perspective: "1000px" }}
+        <div className="w-full h-80 mt-30 sm:h-96 my-20 flex items-center justify-center">
+          <div
+            className="relative w-[180px] sm:w-[200px] md:w-[250px] h-full"
+            style={{ perspective: "1000px" }}
+          >
+            <motion.div
+              animate={{ rotateY: 360 }}
+              transition={{ repeat: Infinity, ease: "linear", duration: 50 }}
+              className="relative w-full h-full transform-3d"
             >
-              <motion.div
-                animate={{ rotateY: 360 }}
-                transition={{ repeat: Infinity, ease: "linear", duration: 50 }}
-                className="relative w-full h-full transform-3d"
-              >
-                {categories.map((cat, i) => {
-                  const angle = (360 / categories.length) * i;
-                  return (
-                    <div
-                      key={i}
-                      className="absolute top-0 left-0 w-full h-full flex flex-col items-center justify-center"
-                      style={{
-                        transform: `rotateY(${angle}deg) translateZ(280px)`,
-                      }}
-                    >
-                      <div className="w-56 h-80 bg-white/10 backdrop-blur-xl rounded-2xl overflow-hidden shadow-xl transition-transform duration-500 hover:scale-110">
-                        <Link href="/tour">
-                          <img
-                            src={cat.img}
-                            className="w-full h-full object-cover"
-                          />
-                        </Link>
-                      </div>
-                      <p className="mt-3 text-xl font-medium">{cat.title}</p>
+              {categories.map((cat, i) => {
+                const angle = (360 / categories.length) * i;
+                return (
+                  <div
+                    key={i}
+                    className="absolute  w-full h-full flex flex-col items-center justify-center"
+                    style={{
+                      transform: `rotateY(${angle}deg) translateZ(280px)`,
+                    }}
+                  >
+                    <div className="w-44 sm:w-56 h-64 sm:h-80 bg-white/10 backdrop-blur-xl rounded-2xl overflow-hidden shadow-xl transition-transform duration-500 hover:scale-110">
+                      <Link href="/tour">
+                        <img
+                          src={cat.img}
+                          className="w-full h-full object-cover"
+                        />
+                      </Link>
                     </div>
-                  );
-                })}
-              </motion.div>
-            </div>
+                    <p className="mt-2 sm:mt-3 text-sm sm:text-xl font-medium capitalize">
+                      {cat.title}
+                    </p>
+                  </div>
+                );
+              })}
+            </motion.div>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
